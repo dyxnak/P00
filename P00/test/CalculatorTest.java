c895c4b9e370c79857e6edd932383c5d45d62019
21035817 <21035817@myrp.edu.sg>
2022-05-30 12:46:31 +0800

    first push to P00 repository in GitHub

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		int a = 1234;
+		int b = 4321;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		assertEquals (5555, actual);
+	}
+	
+	@Test
+	public void testSubtract() {
+		int a = 10;
+		int b = 5;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a, b);
+		
+		assertEquals (5, actual);
+	}
+	
+	@Test
+	public void testMultiply() {
+		int a = 10;
+		int b = 5;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.multiply(a, b);
+		
+		assertEquals (50, actual);
+	}
+	
+	@Test
+	public void testDivide() {
+		int a = 10;
+		int b = 5;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b);
+		
+		assertEquals (2, actual);
+	}
+
+}
